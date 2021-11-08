9c9491662b777a88fe06cc177d4a966b0de7c9d0
LeandroSB <81298497+LeandroSaltori@users.noreply.github.com>
2021-11-08 12:10:09 -0300

    Update patch-prisma.sh

--- patch-prisma.sh
+++ patch-prisma.sh
@@ -1,5 +1,39 @@
 #!/bin/bash
+versao="1.0"
+clear
+echo "||||||||| ||||||||| |||  ||||||||  ||\\    //|||  ||||||||||"
+echo "|||   ||| |||   |||      |||       |||\\  // |||  |||    |||"
+echo "||||||||| ||||||||| |||  ||||||||  ||| \\//  |||  ||||||||||"
+echo "|||       ||| \\\   |||       |||  |||       |||  |||    |||"
+echo "|||       |||  \\\  |||  ||||||||  |||       |||  |||    |||"
+echo "==========================================================="
+echo "Patch Prisma Telecom para Issabel"
+echo "Autor Leandro Saltori - Empresa Prisma Telecom - Franca"
+echo "https://www.prismatelecom.com / 016 3702 - 7844"
+echo "==========================================================="
+echo ""
+sleep 20
+echo ""
+echo "INICIANDO O PROCESSO..."
+echo ""
+echo "Instalando ferramentas úteis..."
+echo ""
 yum install wget tcpdump -y
 updatedb
+echo ""
+echo "Atualizando o sistema..."
+echo ""
 yum -y update && yum -y upgrade
 clear
+echo "||||||||| ||||||||| |||  ||||||||  ||\\    //|||  ||||||||||"
+echo "|||   ||| |||   |||      |||       |||\\  // |||  |||    |||"
+echo "||||||||| ||||||||| |||  ||||||||  ||| \\//  |||  ||||||||||"
+echo "|||       ||| \\\   |||       |||  |||       |||  |||    |||"
+echo "|||       |||  \\\  |||  ||||||||  |||       |||  |||    |||"
+echo "==========================================================="
+echo "Patch Prisma Telecom para Issabel"
+echo "Autor Leandro Saltori - Empresa Prisma Telecom - Franca"
+echo "https://www.prismatelecom.com / 016 3702 - 7844"
+echo "==========================================================="
+echo ""
+echo "===================  FIM  ================================="
