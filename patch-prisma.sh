c64ac384e91ef0b84f2218b8f7c38208f1b0c738
LeandroSB <81298497+LeandroSaltori@users.noreply.github.com>
2021-11-08 12:00:51 -0300

    Update patch-prisma.sh

--- patch-prisma.sh
+++ patch-prisma.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 versao="1.0"
 clear
-echo "Instalando ferramentas úteis..."
-echo ""
 yum install wget tcpdump -y
 updatedb
-echo ""
-echo "Atualizando o sistema..."
-echo ""
 yum -y update && yum -y upgrade
 clear
