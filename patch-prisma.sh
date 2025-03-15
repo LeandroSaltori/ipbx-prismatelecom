ae12565fb00df07dd94ba0cf2c858ee26dec78f3
LeandroSB <81298497+LeandroSaltori@users.noreply.github.com>
2025-03-15 08:27:52 -0300

    Update patch-prisma.sh
    Correção

--- patch-prisma.sh
+++ patch-prisma.sh
@@ -48,7 +48,7 @@ echo ""
 echo "Baixando o Favicon.."
 echo ""
 cd /var/www/html/ 
-mv favicon.ico favcon_old.icon
+mv favicon.ico favcon_old.ico
 cd
 echo ""
 wget -c -P /var/www/html  https://github.com/LeandroSaltori/ipbx-prismatelecom/blob/main/web/favicon.ico
